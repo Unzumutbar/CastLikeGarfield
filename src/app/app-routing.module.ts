0aa2445eddb5bc96fc654eeb9aacb662993c6b30
Unzumutbar <nilsbuergel@gmail.com>
2020-08-11 02:12:21 +0200

    Initial Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'card-list',
+    loadChildren: () =>
+      import('./pages/card-list/card-list.module').then(
+        (m) => m.CardListPageModule
+      ),
+  },
+  {
+    path: 'random-card',
+    loadChildren: () =>
+      import('./pages/random-card/random-card.module').then(
+        (m) => m.RandomCardPageModule
+      ),
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
